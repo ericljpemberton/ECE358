406191c0cce060e1a715b40b662c519921870ed3
Steven Locke <s3locke@uwaterloo.ca>
2016-07-26 22:10:18 -0400

    Big changes.  Manager now only keeps track of a bunch of sockets.  Connecting works.  Client can send and receiver will receive but then they both hang trying to receive an ack from each other.

--- A5/manager.cc
+++ A5/manager.cc
@@ -1,22 +1,4 @@
-#ifndef TEMPTEMPTEMPTEMP
-#define TEMPTEMPTEMPTEMP
-
-
-#include <iostream>
-#include <cstdlib>
-#include <strings.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h> //close
-//TODO: trim above
-
 #include <vector>
-#include "net_util.h"
-#include "ucp.h"
-#include "pickIp.h"
 
 #define NEWCONN 0
 #define BOUND 1
@@ -30,119 +12,30 @@
 #define milliSecs 1000
 
 
-struct RcsConnection {
-    int status;                     //current of rcs socket
-    struct sockaddr_in *UcpTarget;  //target we want to send and receive packets with
-    int UcpRcsPairing;              //index of far side
+class Socket {
+    public:
+        int sockfd;
+        struct sockaddr_in *addr;
+        int expectedAck;
+        int sequenceNumber;
+        int receiving;
 
-    RcsConnection(int state) {
-        status = state;
-        UcpRcsPairing = -1;
-    }
+        Socket(int sockfd) :  sockfd(sockfd), addr(), expectedAck(0), sequenceNumber(0), receiving(0) {
+
+        }
 };
 
-struct Message {
-    int indexMessageIsFor;  //index
-    void *buf;              //contents of message (no footer)
-    int len;                //number of received bytes
-
-    Message(int length) {
-        len = length;
-    }
-};
 
 class Manager {
-    int sockfd;                 //UCP Socket for entire client
-    struct sockaddr_in addr;    //info for UCP socket binding
-
-    std::vector<RcsConnection> registered;
-    std::vector<Message*> queue;
-
 public:
+    std::vector<Socket> sockets;
+
     Manager() {
-
-        sockfd = ucpSocket();
-        if (sockfd < 0) {
-            //perror("ERROR opening socket");
-            exit(1);
-        }
-
-        bzero((char *) &addr, sizeof(addr));
-        addr.sin_family = AF_INET;
-        if (pickServerIPAddr(&addr.sin_addr) < 0) {
-            //perror("ERROR picking ip");
-            exit(1);
-        }
-
-        if (ucpBind(sockfd, &addr) < 0) {
-            //perror("ERROR on binding");
-            exit(1);
-        }
-
-        ucpSetSockRecvTimeout(sockfd, milliSecs);
-
-        //TODO: fork a thread to constantly consume messages
-        //TODO: figure out how to terminate this... since the process needs to die when the client is done
-        /*
-        while (true) {
-            receiveMessages();
-        }
-            */
-
     }
 
-    void receiveMessages() {
-        //TODO 
-        int index;
-        void *buf;
-        int len;
-        int bytes = ucpRecvFrom(sockfd, buf, len, registered[index].UcpTarget);
-        Message *m = new Message(bytes);
-        if (bytes > len); //do something?
-        memcpy(m->buf, buf, len);
-        queue.push_back(m);
+    int registerRcsConnection(int sockfd) {
+        Socket newSock = Socket(sockfd);
+        sockets.push_back(newSock);
+        return (sockets.size() - 1);
     }
-
-    int checkMessageQueue(int index, void *buf, int len) {
-        for (int i = 0; i < queue.size(); ++i) {
-            //if len < message.len
-            if (queue[i]->indexMessageIsFor == index) {
-                changeRcsConnectionState(index, CONNECTED);
-                memcpy(buf, queue[i]->buf, queue[i]->len);
-                int temp = queue[i]->len;
-                queue.erase(queue.begin()+i);
-                return temp;
-            }
-        }
-        return -1; //no message in queue
-    }
-
-    int registerRcsConnection() {
-        //TODO: validate target
-        registered.push_back(RcsConnection(NEWCONN));
-        return registered.size() - 1;
-    }
-
-    void registerRcsUcpTarget(int index, struct sockaddr_in *target) {
-        registered[index].UcpTarget = target;
-    }
-
-    struct sockaddr_in getSockAddrIn() {
-        return addr;
-    }
-
-    void changeRcsConnectionState (int index, int state) {
-        registered[index].status = state;
-    }
-
-    int getRcsConnectionState(int index) {
-        return registered[index].status;
-    }
-
 };
-
-
-
-
-
-#endif // ! TEMPTEMPTEMPTEMP
--- A5/rcs.c
+++ A5/rcs.c
@@ -1,16 +1,28 @@
 #include "rcs.h"
+#include "ucp.h"
 #include "manager.cc"
+#include <cstring>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
 
 //used to allocate an RCS socket
 //Returns a socket descriptor (positive integer) on success
 
 static Manager manager;
 
+int checksum(char *buf) {
+    //TODO
+    return 0;
+}
+
 //used to allocate an RCS socket. No arguments.
 //Returns a socket descriptor (positive integer) on success
 int rcsSocket() 
 {
-    return manager.registerRcsConnection();
+    std::cout << "Making a socket" << std::endl;
+    int sockfd = ucpSocket();
+    return manager.registerRcsConnection(sockfd);
 }
 
 //binds an RCS socket (first argument) to the address structure (second argument)
@@ -18,26 +30,30 @@ int rcsSocket()
 //Returns 0 on success
 int rcsBind(int index, struct sockaddr_in *addr) 
 {
-    manager.changeRcsConnectionState(index, BOUND);
+    std::cout << "Binding a socket" << std::endl;
+    // manager.changeRcsConnectionState(index, BOUND);
     //TODO: how the fuck do we determine the port?
     //addr->sin_port = 
-    manager.registerRcsUcpTarget(index, addr);
-    return 0;
+    // manager.registerRcsUcpTarget(index, addr);
+    manager.sockets.at(index).addr = addr;
+    return ucpBind(manager.sockets.at(index).sockfd, addr);
 }
 
 //fills in the address information into the second argument with which an RCS socket (first argument) has been bound via a call to rcsBind()
 //Returns 0 on success
 int rcsGetSockName(int index, struct sockaddr_in *addr) 
 {
-    addr->sin_addr.s_addr = manager.getSockAddrIn().sin_addr.s_addr;
-    return 0;
+    std::cout << "Getting socket name" << std::endl;
+    addr->sin_addr.s_addr = manager.sockets.at(index).addr->sin_addr.s_addr;
+    return ucpGetSockName(manager.sockets.at(index).sockfd, addr);
 }
 
 //marks an RCS socket (the argument) as listening for connection requests
 //Returns 0 on success
 int rcsListen(int index)
 {
-    manager.changeRcsConnectionState(index, LISTENING);
+    std::cout << "Listening..." << std::endl;
+    manager.sockets.at(index).receiving = 1;
     return 0; 
 }
 
@@ -47,14 +63,50 @@ int rcsListen(int index)
 //Returns a descriptor to a new RCS socket that can be used to rcsSend() and rcsRecv() with the client
 int rcsAccept(int index, struct sockaddr_in *addr)
 {
-    manager.changeRcsConnectionState(index, ACCEPTING);
-
-    while (manager.getRcsConnectionState(index) == ACCEPTING) {
-        //TODO: do handshake
-        //TODO: should set registered.UcpRcsPairing to be index of far side
+    std::cout << "Accepting" << std::endl;
+    //Test if currently receiving
+    if (manager.sockets.at(index).receiving < 1) {
+        return -1;
     }
-    if (manager.getRcsConnectionState(index) != CONNECTED) return -1; //handshake did not succeed
-    return index;
+    
+    char buf[8];
+    int syn = 0;
+
+
+
+    //While we haven't received a syn yet...
+    while (syn <= 0 || strcmp(buf, "syn") != 0) {
+        std::cout << "Waiting for syn from client " << std::endl;
+        syn = ucpRecvFrom(manager.sockets.at(index).sockfd, buf, 4, addr);
+        std::cout << "Server got: " << syn << " as syn" << std::endl;
+    }
+
+    std::cout << "Syn'd from server " << syn << std::endl;
+
+    int returnSock = rcsSocket();
+    manager.sockets.at(returnSock).addr = addr;
+
+    int synack = 0;
+    //Respond with a synack
+    while (synack <= 0) {
+        std::cout << "Trying to synack serverside " << std::endl;
+        synack = ucpSendTo(manager.sockets.at(returnSock).sockfd, "synack", 8, addr);
+    }
+
+    std::cout << "SynAck'd from server " << synack << std::endl;
+
+    int ack = 0;
+    //While we haven't received an ack yet
+    while (ack <= 0 || strcmp(buf, "ack") != 0) {
+        std::cout << "Trying to ack serverside " << std::endl;
+        ack = ucpRecvFrom(manager.sockets.at(returnSock).sockfd, buf, 4, addr);
+    }
+
+    std::cout << "Ack'd from server " << std::endl;
+
+
+    return returnSock;
+
 }
 
 //connects a client to a server
@@ -63,12 +115,29 @@ int rcsAccept(int index, struct sockaddr_in *addr)
 //Returns 0 on success
 int rcsConnect(int index, const struct sockaddr_in *addr)
 {
-    manager.changeRcsConnectionState(index, CONNECTING);
+    std::cout << "Connecting" << std::endl;
+    char buf[8];
+    int syn = ucpSendTo(manager.sockets.at(index).sockfd, "syn", 4, (sockaddr_in*)addr);
+    //Send a syn to start the connection
 
-    while (manager.getRcsConnectionState(index) == CONNECTING) {
-        //TODO: do handshake
+    std::cout << "Syn'd from client " << syn << std::endl;
+     
+    int synack = 0;
+    while(synack <= 0 || strcmp((char *)buf, "synack") != 0) {
+        std::cout << "Trying to synack from client " << std::endl;
+        synack = ucpRecvFrom(manager.sockets.at(index).sockfd, buf, 8, (sockaddr_in*)addr);
     }
-    if (manager.getRcsConnectionState(index) != CONNECTING) return -1; //handshake did not succeed
+
+    std::cout << "SynAck'd from client " << synack << std::endl;
+
+    int ack = 0;
+    while (ack <= 0) {
+        std::cout << "Trying to ack clientside " << std::endl;
+        ack = ucpSendTo(manager.sockets.at(index).sockfd, "ack", 4, (sockaddr_in*)addr);
+    }
+
+    std::cout << "Ack'd from client " << std::endl;
+
     return 0;
 }
 
@@ -81,13 +150,65 @@ int rcsConnect(int index, const struct sockaddr_in *addr)
 //Data is sent and received reliably, so any byte that is returned by this call should be what was sent, and in the correct order
 int rcsRecv(int index, void *buf, int len)
 {
-    manager.changeRcsConnectionState(index, RECEIVING);
-    int bytes = manager.checkMessageQueue(index, buf, len);
-    while (bytes < 0) {
-        bytes = manager.checkMessageQueue(index, buf, len);
-    }
+    std::cout << "Receiving" << std::endl;
+    char* buffer;
 
-    return bytes;
+    while(true) {
+        int numRec = ucpRecvFrom(manager.sockets.at(index).sockfd, buf, len, manager.sockets.at(index).addr);
+        std::cout << "Received " << numRec << " bytes" << std::endl;
+        if (numRec <= 0) {
+            continue;
+        }
+
+        ((char *)buf)[numRec] = 0;
+
+
+        //split buf into sequenceNumber, checksum, message
+        //Check if sequence number is what we expect:
+        char *receivedSequence = strtok((char *)buf, ";");
+        if (receivedSequence <= 0) {
+            continue;
+        }
+        int sequenceRec = atoi(receivedSequence);
+        if (sequenceRec != manager.sockets.at(index).sequenceNumber) {
+            continue;
+        }
+
+        //Expect to receive the next sequence number next time
+        manager.sockets.at(index).sequenceNumber++;
+        //SequenceNumber passes
+
+        char *checkSum = strtok(0, ";");
+        if (checkSum == 0) {
+            continue;
+        }
+
+        int check = atoi(checkSum);
+        //Not 0, will check if it matches once we receive the data
+
+        char *message = strtok(0, ";");
+        if (message == 0 || check != checksum(message)) {
+            continue;
+        }
+
+        int BytesReceived = strlen(message);
+
+        //Message is valid, and in order
+        //Now send an ack for it.
+        std::stringstream sending;
+        sending << manager.sockets.at(index).expectedAck++ << ";"; //Next ack
+        sending << "123" << ";"; //Checksum doesn't matter for the ack
+        sending << "ack";
+
+        int numSent = 0;
+        while (numSent <= 0) {
+            numSent = ucpSendTo(manager.sockets.at(index).sockfd, sending.str().c_str(), strlen(sending.str().c_str()), manager.sockets.at(index).addr);
+        }
+
+        std::cout << "Got: " << message << std::endl;
+
+        return strlen(message);
+    }
 }
 
 //blocks sending data
@@ -97,17 +218,54 @@ int rcsRecv(int index, void *buf, int len)
 //If rcsSend() returns with a non-negative return value, then we know that so many bytes were reliably received by the other end
 int rcsSend(int index, void *buf, int len)
 {
-    //TODO TODO
-    //return (int) send(index, buf, len, 0);
-    // ^ illegal
-    return 0;
+    std::cout << "Trying to send: " << (char*)buf << std::endl;
+
+
+    //Wrap the data to send with a checksum and sequence number.
+    //udpSend it
+    //Wait for ack
+    //If no ack, resend
+    //Return the length of the bytes sent
+
+    std::stringstream sending;
+    sending << manager.sockets.at(index).sequenceNumber++ << ";";
+    sending << checksum((char *)buf) << ";";
+    sending << (char *)buf;
+
+    int numSent = 0;
+    int numRec = 0;
+    char* recv;
+    char* ack;
+
+    while(true) {
+        //While not acked, try to send it
+        numSent = ucpSendTo(manager.sockets.at(index).sockfd, sending.str().c_str(), strlen(sending.str().c_str()), manager.sockets.at(index).addr);
+        while (numSent <= 0) {
+            numSent = ucpSendTo(manager.sockets.at(index).sockfd, sending.str().c_str(), strlen(sending.str().c_str()), manager.sockets.at(index).addr);
+        }  
+
+        numRec = rcsRecv(index, recv, 64);
+        recv[numRec] = 0;
+
+        strtok(recv, ";"); //Sequence number
+        strtok(NULL, ";"); //Checksum
+        ack = strtok(NULL, ";");
+        if (ack == NULL || strcmp(ack, "ack") != 0) {
+            continue;
+        }
+
+        return numSent;
+    }
+
+    return -1;
 }
 
 //closes an RCS socket descriptor
 //Returns 0 on success.
 int rcsClose(int index)
 {
+    std::cout << "Closing" << std::endl;
     //TODO: do handshake
-    manager.changeRcsConnectionState(index, CLOSED);
+    // manager.changeRcsConnectionState(index, CLOSED);
     return 0;
 }
